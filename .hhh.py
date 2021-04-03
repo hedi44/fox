e482af2df1a05acacfdb87a30ccfb51a504efbb8
hedi44 <69325648+hedi44@users.noreply.github.com>
2021-04-03 13:19:21 +0300

    Update .hhh.py

--- .hhh.py
+++ .hhh.py
@@ -86,7 +86,7 @@ def menu():
     time.sleep(2)
     print("\033[32m.")
     os.system("clear")
-    os.system("figlet Type; espeak -a 150 type 123")
+    os.system("figlet Type; espeak -a 150 type123")
     print("\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\033[0;1m")
     print("   Auther: Zed\n   Chenall telegram: @Zed_Cracker_1\n   bnwsa 123")
  
